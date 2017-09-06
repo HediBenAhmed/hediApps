fdbb3a8971caab1b6c717cebf002e0a58dd5cf77
HediBenAhmed <hedi.bahmed@gmail.com>
2017-09-06 09:55:06 +0200

    add hediapps-web module

---
+++ hediapps-web/e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { HediappsWebPage } from './app.po';
+
+describe('hediapps-web App', () => {
+  let page: HediappsWebPage;
+
+  beforeEach(() => {
+    page = new HediappsWebPage();
+  });
+
+  it('should display welcome message', done => {
+    page.navigateTo();
+    page.getParagraphText()
+      .then(msg => expect(msg).toEqual('Welcome to app!!'))
+      .then(done, done.fail);
+  });
+});
