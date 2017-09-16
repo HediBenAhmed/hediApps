072af83814c743780079456d2b9328eef661c8b9
HediBenAhmed <hedi.bahmed@gmail.com>
2017-09-16 14:27:53 +0200

    remove unused modules

--- hediapps-webapp/src/main/java/com/hediapps/config/SpringSecurityInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.hediapps.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends
-		AbstractSecurityWebApplicationInitializer {
-	// do nothing
-}
