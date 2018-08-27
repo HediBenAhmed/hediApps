c3e048db55c5bfd8ca17dde71005b310a162d092
Hedi Ben-Ahmed <hedi.benahmed@vo2-group.com>
2018-08-27 16:31:58 +0200

    [FEATURE] Migrate to gradle.

--- hediapps-users/src/main/java/com/hediapps/users/UsersApplication.java
+++ hediapps-users/src/main/java/com/hediapps/users/UsersApplication.java
@@ -2,12 +2,10 @@ package com.hediapps.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableOAuth2Sso
 public class UsersApplication {
 
 	public static void main(String[] args) {
